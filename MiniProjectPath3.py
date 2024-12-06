e1bf067e0ec3a6400fa951d7162a0c24a50116f4
wangsam900331 <93728024+wangsam900331@users.noreply.github.com>
2024-12-06 11:15:44 -0500

    .

--- MiniProjectPath3.py
+++ MiniProjectPath3.py
@@ -23,18 +23,30 @@ X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.
 def dataset_searcher(number_list,images,labels):
   #insert code that when given a list of integers, will find the labels and images
   #and put them all in numpy arrary (at the same time, as training and testing data)
-
+  images_list = []
+  labels_list = []
+  for num in number_list:
+        indices = np.where(labels == num)[0]
+        images_list.extend(images[indices])
+        labels_list.extend(labels[indices])
+  return np.array(images_list), np.array(labels_list)
   pass
   #return images_nparray, labels_nparray
 
 def print_numbers(images,labels):
   #insert code that when given images and labels (of numpy arrays)
-  #the code will plot the images and their labels in the title. 
-  pass
+  #the code will plot the images and their labels in the title.
+  for i, image in enumerate(images[:len(labels)]):
+      plt.subplot(1, len(labels), i + 1)
+      plt.imshow(image, cmap='gray')
+      plt.title(f"Label: {labels[i]}")
+      plt.axis('off')
+  plt.show() 
+  #pass
 
 class_numbers = [2,0,8,7,5]
 #Part 1
-class_number_images , class_number_labels = dataset_searcher(class_numbers)
+class_number_images , class_number_labels = dataset_searcher(class_numbers, images, labels)
 #Part 2
 print_numbers(class_number_images , class_number_labels )
 
@@ -45,16 +57,19 @@ model_1 = GaussianNB()
 # so instead of having the Xtrain data beign of shape 718 (718 images) by 8 by 8
 # the new shape would be 718 by 64
 X_train_reshaped = X_train.reshape(X_train.shape[0], -1)
+X_test_reshaped = X_test.reshape(X_test.shape[0], -1)
 
 #Now we can fit the model
 model_1.fit(X_train_reshaped, y_train)
 #Part 3 Calculate model1_results using model_1.predict()
-model1_results = model_1.predict('''What should go in here? Hint, look at documentation and some reshaping may need to be done''')
+model1_results = model_1.predict(X_test_reshaped)
 
 
 def OverallAccuracy(results, actual_values):
   #Calculate the overall accuracy of the model (out of the predicted labels, how many were correct?)
-  
+  correct = np.sum(results == actual_values)
+  total = len(actual_values)
+  Accuracy =  correct / total
   return Accuracy
 
 
@@ -72,11 +87,17 @@ allnumbers_images, allnumbers_labels = dataset_searcher(allnumbers)
 #Part 6
 #Repeat for K Nearest Neighbors
 model_2 = KNeighborsClassifier(n_neighbors=10)
-
+model_2.fit(X_train_reshaped, y_train)
+model2_results = model_2.predict(X_test_reshaped)
+Model2_Overall_Accuracy = OverallAccuracy(model2_results, y_test)
+print("The overall accuracy of the KNN model is " + str(Model2_Overall_Accuracy))
 
 #Repeat for the MLP Classifier
-model_3 = MLPClassifier(random_state=0)
-
+model_3 = MLPClassifier(random_state=0,max_iter=500)
+model_3.fit(X_train_reshaped, y_train)
+model3_results = model_3.predict(X_test_reshaped)
+Model3_Overall_Accuracy = OverallAccuracy(model3_results, y_test)
+print("The overall accuracy of the MLP model is " + str(Model3_Overall_Accuracy))
 
 
 #Part 8
@@ -90,7 +111,26 @@ X_train_poison = X_train + poison
 
 #Part 9-11
 #Determine the 3 models performance but with the poisoned training data X_train_poison and y_train instead of X_train and y_train
+# Poisoned data
+X_train_poison_reshaped = X_train_poison.reshape(X_train_poison.shape[0], -1)
 
+# GaussianNB with poisoned data
+model_1.fit(X_train_poison_reshaped, y_train)
+model1_poisoned_results = model_1.predict(X_test_reshaped)
+Model1_Poisoned_Accuracy = OverallAccuracy(model1_poisoned_results, y_test)
+print("Gaussian model accuracy on poisoned data: " + str(Model1_Poisoned_Accuracy))
+
+# KNeighborsClassifier with poisoned data
+model_2.fit(X_train_poison_reshaped, y_train)
+model2_poisoned_results = model_2.predict(X_test_reshaped)
+Model2_Poisoned_Accuracy = OverallAccuracy(model2_poisoned_results, y_test)
+print("KNN model accuracy on poisoned data: " + str(Model2_Poisoned_Accuracy))
+
+# MLPClassifier with poisoned data
+model_3.fit(X_train_poison_reshaped, y_train)
+model3_poisoned_results = model_3.predict(X_test_reshaped)
+Model3_Poisoned_Accuracy = OverallAccuracy(model3_poisoned_results, y_test)
+print("MLP model accuracy on poisoned data: " + str(Model3_Poisoned_Accuracy))
 
 
 #Part 12-13
@@ -99,7 +139,31 @@ X_train_poison = X_train + poison
 # When fitting the KernelPCA method, the input image of size 8x8 should be reshaped into 1 dimension
 # So instead of using the X_train_poison data of shape 718 (718 images) by 8 by 8, the new shape would be 718 by 64
 
-X_train_denoised = # fill in the code here
+#X_train_denoised = # fill in the code here
+from sklearn.decomposition import KernelPCA
+
+# Denoise poisoned data
+X_train_poison_reshaped = X_train_poison.reshape(X_train_poison.shape[0], -1)
+kpca = KernelPCA(n_components=64, kernel='rbf', gamma=0.1)
+X_train_denoised = kpca.fit_transform(X_train_poison_reshaped)
+
+# Train GaussianNB with denoised data
+model_1.fit(X_train_denoised, y_train)
+model1_denoised_results = model_1.predict(X_test_reshaped)
+Model1_Denoised_Accuracy = OverallAccuracy(model1_denoised_results, y_test)
+print("Gaussian model accuracy on denoised data: " + str(Model1_Denoised_Accuracy))
+
+# Train KNN with denoised data
+model_2.fit(X_train_denoised, y_train)
+model2_denoised_results = model_2.predict(X_test_reshaped)
+Model2_Denoised_Accuracy = OverallAccuracy(model2_denoised_results, y_test)
+print("KNN model accuracy on denoised data: " + str(Model2_Denoised_Accuracy))
+
+# Train MLPClassifier with denoised data
+model_3.fit(X_train_denoised, y_train)
+model3_denoised_results = model_3.predict(X_test_reshaped)
+Model3_Denoised_Accuracy = OverallAccuracy(model3_denoised_results, y_test)
+print("MLP model accuracy on denoised data: " + str(Model3_Denoised_Accuracy))
 
 
 #Part 14-15
